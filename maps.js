a2c9b7be16be705e9966fffeba007c5bf034f307
kathyorr7152 <katbatorr@gmail.com>
2016-08-17 09:51:38 -0700

    adding safe space markers

--- maps.js
+++ maps.js
@@ -36,3 +36,33 @@ if ("geolocation" in navigator){ //check geolocation available
 }else{
     console.log("Browser doesn't support geolocation!");
 }
+
+The Lobby Bar	1505 10th Avenue, Seattle, WA , 98122, United States	47.614336	-122.319785
+
+//marker code!!
+var markersData = [
+  {
+      lat: 47.614336,
+      lng: -122.319785,
+      name: "The Lobby Bar",
+      address1:"1505 10th Avenue",
+      address2: "Seattle, WA",
+      postalCode: "98122, United States" // don’t insert comma in the last item of each marker
+   },
+   {
+      lat: 40.59955,
+      lng: -8.7498167,
+      name: "Camping Costa Nova",
+      address1:"Quinta dos Patos, n.º 2",
+      address2: "Praia da Costa Nova",
+      postalCode: "3830-453 Gafanha da Encarnação" // don’t insert comma in the last item of each marker
+   },
+   {
+      lat: 40.6247167,
+      lng: -8.7129167,
+      name: "Camping Gafanha da Nazaré",
+      address1:"Rua dos Balneários do Complexo Desportivo",
+      address2: "Gafanha da Nazaré",
+      postalCode: "3830-225 Gafanha da Nazaré" // don’t insert comma in the last item of each marker
+   } // don’t insert comma in last item
+];
