3f604ede675ad683db78151b3286c2ca25c8b9d3
kathyorr7152 <katbatorr@gmail.com>
2016-08-18 16:58:10 -0700

    Working Info Box
    infoWindows is a list where you can put contentString s with titles!!
    Syd will help you with this if you need it!!

--- maps.js
+++ maps.js
@@ -1,210 +1,309 @@
-      var map;
-      function initMap() {
-        map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 12,
-          center: new google.maps.LatLng(47.614336,-122.319785),
-          mapTypeId: 'roadmap',
+var map;
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 12,
+    center: new google.maps.LatLng(47.614336,-122.319785),
+    mapTypeId: 'roadmap',
+  });
+
+{
+
+
+// Note: This example requires that you consent to location sharing when
+// prompted by your browser. If you see the error "The Geolocation service
+// failed.", it means you probably did not give permission for the browser to
+// locate you.
+
+  var geoLocationWindow = new google.maps.InfoWindow({map: map});
+  // Try HTML5 geolocation.
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+
+      geoLocationWindow.setPosition(pos);
+      geoLocationWindow.setContent('You are here!');
+      map.setCenter(pos);
+    }, function() {
+      handleLocationError(true, geoLocationWindow, map.getCenter());
+    });
+  } else {
+    // Browser doesn't support Geolocation
+    handleLocationError(false, geoLocationWindow, map.getCenter());
+  }
+}
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(browserHasGeolocation ?
+                        'Error: The Geolocation service failed.' :
+                        'Error: Your browser doesn\'t support geolocation.');
+}
+
+  var iconBase = 'https://maps.google.com/mapfiles/kml/shapes/';
+  var icons = {
+    parking: {
+      icon: iconBase + 'parking_lot_maps.png'
+    },
+    library: {
+      icon: iconBase + 'library_maps.png'
+    },
+    info: {
+      icon:'star.png'
+    }
+  };
+
+  function addMarker(feature, infoWindow) {
+    var marker = new google.maps.Marker({
+      position: feature.position,
+      icon: icons[feature.type].icon,
+      map: map
+    });
+    marker.addListener('click', function() {
+    infoWindow.open(map, marker);
+  });
+  }
+
+  var features = [
+    {
+    //The Lobby Bar
+      position: new google.maps.LatLng(47.614336, -122.319785),
+      type: 'library'
+    }, {
+    //Gay City Health Project
+      position: new google.maps.LatLng( 47.613912,-122.324633),
+      type: 'parking'
+    }, {
+    //STARBUCKS
+    //1
+      position: new google.maps.LatLng(47.614028, -122.337109),
+      type: 'info'
+    }, {
+    //2
+      position: new google.maps.LatLng(47.61018,-122.342392),
+      type: 'info'
+    }, {
+     //3
+      position: new google.maps.LatLng(47.662128, -122.296273),
+      type: 'info'
+    }, {
+    //4
+      position: new google.maps.LatLng(47.608363,-122.338488),
+      type: 'info'
+    }, {
+    //5
+      position: new google.maps.LatLng(47.620481,-122.313144),
+      type: 'info'
+    }, {
+    //6
+      position: new google.maps.LatLng(47.618231,-122.303942),
+      type: 'info'
+    }, {
+    //7
+      position: new google.maps.LatLng(47.630489,-122.32231),
+      type: 'info'
+    }, {
+      //8
+      position: new google.maps.LatLng(47.607634,-122.310118),
+       type: 'info'
+    }, {
+      //9
+      position: new google.maps.LatLng(47.661828,-122.33624),
+      type: 'info'
+    }, {
+      //10
+      position: new google.maps.LatLng(47.691889,-122.359125),
+      type: 'info'
+    }, {
+      //11
+      position: new google.maps.LatLng(47.637722,-122.356146),
+      type: 'info'
+    }, {
+      //12
+      position: new google.maps.LatLng(47.68966,-122.376136),
+     type: 'info'
+    }, {
+      //13
+      position: new google.maps.LatLng(47.66825,-122.374273),
+     type: 'info'
+    }, { 
+      //14
+      position: new google.maps.LatLng(47.617405,-122.344447),
+      type: 'info'
+    }, {
+      //15
+      position: new google.maps.LatLng(47.622525,-122.321375),
+      type: 'info'
+    }, {
+      //16
+      position: new google.maps.LatLng(47.623822,-122.355237),
+      type: 'info'
+    }, {
+      //17
+      position: new google.maps.LatLng(47.579068,-122.299594),
+      type: 'info'
+    }, {
+      //18
+      position: new google.maps.LatLng(47.579831,-122.38593),
+      type: 'info'
+    }, {
+      //19
+      position: new google.maps.LatLng(47.718018,-122.311915),
+      type: 'info'
+    }, {
+      //20
+      position: new google.maps.LatLng(47.559739,-122.385295),
+      type: 'info'
+    }, {
+      //21
+      position: new google.maps.LatLng(47.677932,-122.354581),
+     type: 'info'
+    }, {
+      //22
+      position: new google.maps.LatLng(47.649861,-122.35007),
+      type: 'info'
+    }, {
+      //23
+      position: new google.maps.LatLng(47.679988,-122.325477),
+      type: 'info'
+    }, {
+      //24
+      position: new google.maps.LatLng(47.6814,-122.290689),
+      type: 'info'
+    }, {
+      //25
+      position: new google.maps.LatLng(47.662626,-122.299708),
+      type: 'info'
+    }, {
+      //26
+      position: new google.maps.LatLng(47.641021,-122.325562),
+      type: 'info'
+    }, {
+    }
+  ];
+
+var infoWindows = [
+    {
+      contentString:'<p>Lobby Bar</p>'
+    }, {
+      contentString:'<p>Gay City Health Project</p>'
+    }, {
+    //STARBUCKS
+    //1
+      position: new google.maps.LatLng(47.614028, -122.337109),
+      type: 'info'
+    }, {
+    //2
+      position: new google.maps.LatLng(47.61018,-122.342392),
+      type: 'info'
+    }, {
+     //3
+      position: new google.maps.LatLng(47.662128, -122.296273),
+      type: 'info'
+    }, {
+    //4
+      position: new google.maps.LatLng(47.608363,-122.338488),
+      type: 'info'
+    }, {
+    //5
+      position: new google.maps.LatLng(47.620481,-122.313144),
+      type: 'info'
+    }, {
+    //6
+      position: new google.maps.LatLng(47.618231,-122.303942),
+      type: 'info'
+    }, {
+    //7
+      position: new google.maps.LatLng(47.630489,-122.32231),
+      type: 'info'
+    }, {
+      //8
+      position: new google.maps.LatLng(47.607634,-122.310118),
+       type: 'info'
+    }, {
+      //9
+      position: new google.maps.LatLng(47.661828,-122.33624),
+      type: 'info'
+    }, {
+      //10
+      position: new google.maps.LatLng(47.691889,-122.359125),
+      type: 'info'
+    }, {
+      //11
+      position: new google.maps.LatLng(47.637722,-122.356146),
+      type: 'info'
+    }, {
+      //12
+      position: new google.maps.LatLng(47.68966,-122.376136),
+     type: 'info'
+    }, {
+      //13
+      position: new google.maps.LatLng(47.66825,-122.374273),
+     type: 'info'
+    }, { 
+      //14
+      position: new google.maps.LatLng(47.617405,-122.344447),
+      type: 'info'
+    }, {
+      //15
+      position: new google.maps.LatLng(47.622525,-122.321375),
+      type: 'info'
+    }, {
+      //16
+      position: new google.maps.LatLng(47.623822,-122.355237),
+      type: 'info'
+    }, {
+      //17
+      position: new google.maps.LatLng(47.579068,-122.299594),
+      type: 'info'
+    }, {
+      //18
+      position: new google.maps.LatLng(47.579831,-122.38593),
+      type: 'info'
+    }, {
+      //19
+      position: new google.maps.LatLng(47.718018,-122.311915),
+      type: 'info'
+    }, {
+      //20
+      position: new google.maps.LatLng(47.559739,-122.385295),
+      type: 'info'
+    }, {
+      //21
+      position: new google.maps.LatLng(47.677932,-122.354581),
+     type: 'info'
+    }, {
+      //22
+      position: new google.maps.LatLng(47.649861,-122.35007),
+      type: 'info'
+    }, {
+      //23
+      position: new google.maps.LatLng(47.679988,-122.325477),
+      type: 'info'
+    }, {
+      //24
+      position: new google.maps.LatLng(47.6814,-122.290689),
+      type: 'info'
+    }, {
+      //25
+      position: new google.maps.LatLng(47.662626,-122.299708),
+      type: 'info'
+    }, {
+      //26
+      position: new google.maps.LatLng(47.641021,-122.325562),
+      type: 'info'
+    }, {
+    }
+  ];
+
+  //As long as infowindows and features have matching indexes and they are exactly the same length this will work
+  for (var i = 0, feature; feature = features[i]; i++) {
+     var infowindow = new google.maps.InfoWindow({
+          content: infoWindows[i].contentString
         });
-
-      {
-        var contentString = '<div id="content">'+
-            '<div id="siteNotice">'+
-            '</div>'+
-            '<h1 id="firstHeading" class="firstHeading">Safe Space!</h1>'+
-            '<div id="bodyContent">'+
-            '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
-            'sandstone rock formation in the southern part of the '+
-            'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
-            'south west of the nearest large town, Alice Springs; 450&#160;km '+
-            '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
-            'features of the Uluru - Kata Tjuta National Park. Uluru is '+
-            'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
-            'Aboriginal people of the area. It has many springs, waterholes, '+
-            'rock caves and ancient paintings. Uluru is listed as a World '+
-            'Heritage Site.</p>'+
-            '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
-            'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
-            '(last visited June 22, 2009).</p>'+
-            '</div>'+
-            '</div>';
+    addMarker(feature, infowindow);
+  }
+}
 
 
-      // Note: This example requires that you consent to location sharing when
-      // prompted by your browser. If you see the error "The Geolocation service
-      // failed.", it means you probably did not give permission for the browser to
-      // locate you.
-
-        var infoWindow = new google.maps.InfoWindow({map: map});
-          content: contentString
-        // Try HTML5 geolocation.
-        if (navigator.geolocation) {
-          navigator.geolocation.getCurrentPosition(function(position) {
-            var pos = {
-              lat: position.coords.latitude,
-              lng: position.coords.longitude
-            };
-
-            infoWindow.setPosition(pos);
-            infoWindow.setContent('You are here!');
-            map.setCenter(pos);
-          }, function() {
-            handleLocationError(true, infoWindow, map.getCenter());
-          });
-        } else {
-          // Browser doesn't support Geolocation
-          handleLocationError(false, infoWindow, map.getCenter());
-        }
-      }
-      function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-        infoWindow.setPosition(pos);
-        infoWindow.setContent(browserHasGeolocation ?
-                              'Error: The Geolocation service failed.' :
-                              'Error: Your browser doesn\'t support geolocation.');
-      }
-        var iconBase = 'https://maps.google.com/mapfiles/kml/shapes/';
-        var icons = {
-          parking: {
-            icon: iconBase + 'parking_lot_maps.png'
-          },
-          library: {
-            icon: iconBase + 'library_maps.png'
-          },
-          info: {
-            icon:'star.png'
-          }
-        };
-
-        function addMarker(feature) {
-          var marker = new google.maps.Marker({
-            position: feature.position,
-            icon: icons[feature.type].icon,
-            map: map
-          });
-          marker.addListener('click', function() {
-          infowindow.open(map, marker);
-        });
-        }
-         var features = [
-          {
-          //The Lobby Bar
-            position: new google.maps.LatLng(47.614336, -122.319785),
-            type: 'library'
-          }, {
-          //Gay City Health Project
-            position: new google.maps.LatLng( 47.613912,-122.324633),
-            type: 'parking'
-          }, {
-          //STARBUCKS
-          //1
-            position: new google.maps.LatLng(47.614028, -122.337109),
-            type: 'info'
-          }, {
-          //2
-            position: new google.maps.LatLng(47.61018,-122.342392),
-            type: 'info'
-          }, {
-           //3
-            position: new google.maps.LatLng(47.662128, -122.296273),
-            type: 'info'
-          }, {
-          //4
-            position: new google.maps.LatLng(47.608363,-122.338488),
-            type: 'info'
-          }, {
-          //5
-            position: new google.maps.LatLng(47.620481,-122.313144),
-            type: 'info'
-          }, {
-          //6
-            position: new google.maps.LatLng(47.618231,-122.303942),
-            type: 'info'
-          }, {
-          //7
-            position: new google.maps.LatLng(47.630489,-122.32231),
-            type: 'info'
-          }, {
-            //8
-            position: new google.maps.LatLng(47.607634,-122.310118),
-             type: 'info'
-          }, {
-            //9
-            position: new google.maps.LatLng(47.661828,-122.33624),
-            type: 'info'
-          }, {
-            //10
-            position: new google.maps.LatLng(47.691889,-122.359125),
-            type: 'info'
-          }, {
-            //11
-            position: new google.maps.LatLng(47.637722,-122.356146),
-            type: 'info'
-          }, {
-            //12
-            position: new google.maps.LatLng(47.68966,-122.376136),
-           type: 'info'
-          }, {
-            //13
-            position: new google.maps.LatLng(47.66825,-122.374273),
-           type: 'info'
-          }, { 
-            //14
-            position: new google.maps.LatLng(47.617405,-122.344447),
-            type: 'info'
-          }, {
-            //15
-            position: new google.maps.LatLng(47.622525,-122.321375),
-            type: 'info'
-          }, {
-            //16
-            position: new google.maps.LatLng(47.623822,-122.355237),
-            type: 'info'
-          }, {
-            //17
-            position: new google.maps.LatLng(47.579068,-122.299594),
-            type: 'info'
-          }, {
-            //18
-            position: new google.maps.LatLng(47.579831,-122.38593),
-            type: 'info'
-          }, {
-            //19
-            position: new google.maps.LatLng(47.718018,-122.311915),
-            type: 'info'
-          }, {
-            //20
-            position: new google.maps.LatLng(47.559739,-122.385295),
-            type: 'info'
-          }, {
-            //21
-            position: new google.maps.LatLng(47.677932,-122.354581),
-           type: 'info'
-          }, {
-            //22
-            position: new google.maps.LatLng(47.649861,-122.35007),
-            type: 'info'
-          }, {
-            //23
-            position: new google.maps.LatLng(47.679988,-122.325477),
-            type: 'info'
-          }, {
-            //24
-            position: new google.maps.LatLng(47.6814,-122.290689),
-            type: 'info'
-          }, {
-            //25
-            position: new google.maps.LatLng(47.662626,-122.299708),
-            type: 'info'
-          }, {
-            //26
-            position: new google.maps.LatLng(47.641021,-122.325562),
-            type: 'info'
-          }, {
-          }
-        ];
-        for (var i = 0, feature; feature = features[i]; i++) {
-          addMarker(feature);
-        }
-      }
-      
-      
-     
