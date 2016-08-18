efbef0f08cc158f6349bb63807ac0722758973e4
Felicityrb <felicitygirl05@gmail.com>
2016-08-18 10:51:00 -0800

    The return of geoloaction

--- maps.js
+++ maps.js
@@ -18,6 +18,24 @@
             icon: iconBase + 'info-i_maps.png'
           }
         };
+                // Try HTML5 geolocation.
+        if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(function(position) {
+            var pos = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude
+            };
+
+            infoWindow.setPosition(pos);
+            infoWindow.setContent('Location found.');
+            map.setCenter(pos);
+          }, function() {
+            handleLocationError(true, infoWindow, map.getCenter());
+          });
+        } else {
+          // Browser doesn't support Geolocation
+          handleLocationError(false, infoWindow, map.getCenter());
+        }
 
         function addMarker(feature) {
           var marker = new google.maps.Marker({
