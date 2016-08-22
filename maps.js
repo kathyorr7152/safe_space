18e7a36cee11b1957de625a642b250cc91cfcc6a
kathyorr7152 <katbatorr@gmail.com>
2016-08-22 15:54:01 -0700

    i have no idea what happened

--- maps.js
+++ maps.js
@@ -525,17 +525,3 @@ var infoWindows = [
     addMarker(feature, infowindow);
 }}
 
-//function findDirection() {
-//  origin: LatLng | String | google.maps.Place,
-//  destination: LatLng | String | google.maps.Place,
- // travelMode: TravelMode,
-//  transitOptions: TransitOptions,
- // drivingOptions: DrivingOptions,
- // unitSystem: UnitSystem,
- // waypoints[]: DirectionsWaypoint,
- // optimizeWaypoints: Boolean,
-//  provideRouteAlternatives: Boolean,
- // avoidHighways: Boolean,
- // avoidTolls: Boolean,
- // region: String
-//}
