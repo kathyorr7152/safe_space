9fc1f5d01a97fc2c8b873b0152c28d40818971f8
kathyorr7152 <katbatorr@gmail.com>
2016-08-22 13:47:28 -0700

    more emergency button junk

--- emergency_btn.js
+++ emergency_btn.js
@@ -1,16 +1,25 @@
+$(function() {
+          if ( !navigator.userAgent.match(/(iPad|iPhone|iPod)/g) ) return;
 
-    $(document).ready(function(){
-    navigator.geolocation.getCurrentPosition(handle_geolocation_query);
-     });
-     
-     function handle_geolocation_query(position){
-     // do something with the position information
-     lat = parseInt(position.coords. latitude*10000,10)/10000;
-     lon = parseInt(position.coords.longitude*10000,10)/10000; 
-     alert(lat + " " + lon);
-     }
+          jQuery('a[href^="sms:"]').attr('href', function() {
+            // Convert: sms:+000?body=example
+            // To iOS:  sms:+000;body=example (semicolon, not question mark)
+            return jQuery(this).attr('href').replace(/sms:(\+?([0-9]*))?\?/, 'sms:$1;');
+                  });
+                })();
+            $(document).ready(function(){
+            navigator.geolocation.getCurrentPosition(handle_geolocation_query);
+             });
+             
+             function handle_geolocation_query(position){
+             // do something with the position information
+             lat = parseInt(position.coords. latitude*10000,10)/10000;
+             lon = parseInt(position.coords.longitude*10000,10)/10000; 
+             alert(lat + " " + lon);
+             }
     
-    $(function() {
+
+/*    $(function() {
         var Geo={};
 
         if (navigator.geolocation) {
@@ -33,5 +42,5 @@
             $('#Long').html(lng);
         }
 
-    });
+    }); */
     
